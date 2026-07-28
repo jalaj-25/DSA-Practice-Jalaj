a4b52d7e8ae0c4bbcd3d7759981fa56c96b6ccad
MILAN SINGHAL <118826647+milansinghal2004@users.noreply.github.com>
2026-07-28 10:43:24 +0530

    Solved Two Sum

---
+++ 2026-07-28/001-two-sum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+
+            if (mp.count(need))
+                return {mp[need], i};
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> ans = obj.twoSum(nums, target);
+
+    cout << ans[0] << " " << ans[1] << endl;
+
+    return 0;
+}
